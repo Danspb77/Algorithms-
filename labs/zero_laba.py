bf93b123e5228ee5cc93a463402a1b08bd9365cb
Danya <dan.shell@bk.ru>
2024-02-12 13:46:51 +0300

    add variables specification

--- labs/zero_laba.py
+++ labs/zero_laba.py
@@ -1,36 +1,41 @@
-
-# a - коэффицент перед х**2
-
-
-
-print("Введите коэффициенты а, b, c")
-
-
-
-a,b,c= map(float,input().split())
+# a - коэффициент перед х**2 (a ∈ R)
+# b - коэффициент перед х, (b ∈ R)
+# c -  свободный коэффициент (c ∈ R)
+# ans - корень уравнения (ans ∈ R)
+# ans1 - корень уравнения (ans1 ∈ R)
+# ans2 - корень уравнения (ans2 ∈ R)
+# D - дискриминант(D ∈ R)
 
 
 
-if a==0:
-    if b==0:
-        if c==0:
+
+
+print("Введите через пробел коэффициенты а, b, c")
+
+
+a, b, c = map(float, input().split())
+
+
+if a == 0:
+    if b == 0:
+        if c == 0:
             print("X- любое число")
         else:
-            print("Решений нет")    
+            print("Решений нет")
     else:
-        ans=-c/b
-        print(ans)
+        ans = -c/b
+        print("Это линейное уравнение", ans)
 
 
 else:
-    D=b**2-4*a*c
-    if D>0:
-        ans1=(-b+D**0.5)/(2*a)
-        ans2=(-b-D**0.5)/(2*a)
-        print (ans1, ans2)
+    D = b**2-4*a*c
+    if D > 0:
+        ans1 = (-b+D**0.5)/(2*a)
+        ans2 = (-b-D**0.5)/(2*a)
+        print("Корни уравнения:",ans1," , ", ans2)
     else:
-        if D==0:
-            ans=-b/(2*a)
-            print(ans)
+        if D == 0:
+            ans = -b/(2*a)
+            print("Дискриминант = 0",ans)
         else:
-            print("Решений нет")    
+            print("Решений нет")
