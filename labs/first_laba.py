13bf1392b854a2d2f86c087efc09875ead7b90ac
Danya <dan.shell@bk.ru>
2024-02-20 12:15:19 +0300

    change to working heap sort

--- labs/first_laba.py
+++ labs/first_laba.py
@@ -1,49 +1,37 @@
-# heap sort
-
 # Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является индексом в arr[]. n - размер кучи
-
 def heapify(arr, n, i):
-    largest = i  # инициализация самого большого элемента как корня
-    l = i*2+1  # left = 2*i + 1
-    r = i*2+2  # right = 2*i + 2
+    largest = i  # Инициализация самого большого элемента как корня
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
 
-    # проверяем существует ли левый дочерний эл. больший корня
-
-    if l < n and arr[l] > arr[i]:
+    # Проверяем, существует ли левый дочерний элемент, больший корня
+    if l < n and arr[l] > arr[largest]:
         largest = l
 
-    # проверяем существует ли правый доч. эл . больший корня
-
-    if r < n and arr[r] > arr[i]:
+    # Проверяем, существует ли правый дочерний элемент, больший корня
+    if r < n and arr[r] > arr[largest]:
         largest = r
 
-    # заменяем корень, если нужно
-
+    # Заменяем корень, если нужно
     if largest != i:
-        arr[largest], arr[i] = arr[i], arr[largest]  # свап
-
-        # применяем  heapify к корню
-
+        arr[i], arr[largest] = arr[largest], arr[i]  # Свап
+        # Применяем heapify к корню
         heapify(arr, n, largest)
 
-# основная функция для сортировки массива заданного размера
+# Основная функция для сортировки массива заданного размера
+def heapSort(arr):
+    n = len(arr)
 
-def heapSort (arr):
-    n =len (arr)
-
-    #  построение max -heap
-    for i in range (n, -1, -1):
+    # Построение max-heap
+    for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
-        
-    # один за другим извлекаем элементы
-    for i in range(n-1 , 0 , -1):
-        arr[i],arr[0]=arr[0],arr[i]
+
+    # Один за другим извлекаем элементы
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # Свап корня с последним элементом
         heapify(arr, i, 0)
 
 # Управляющий код для тестирования
-arr = [ 12, 11, 13, 5, 6, 7]
+arr = [12, 11, 13, 5, 6, 7]
 heapSort(arr)
-n =len(arr)
-print ("Sorted array is")
-for i in range(n):
-    print ("%d" %arr[i])
+print("Sorted array is:", arr)
