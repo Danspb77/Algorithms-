67018cc6ada5cb968f15377dc940db1894b7fc57
Danya <dan.shell@bk.ru>
2024-04-19 14:59:15 +0300

    add comments

--- labs/fourth.c
+++ labs/fourth.c
@@ -15,15 +15,68 @@ double legendre(int degree, double x) {
 
 // Функция для вычисления коэффициентов полинома методом наименьших квадратов
 void least_squares_prediction(double *x, double *y, int n, int degree, double *coefficients) {
-    // Оставляем без изменений, реализация остается та же
-    // ...
+    // Создаем матрицу дизайна, где каждый элемент [i][j] содержит значение полинома Лежандра степени j для точки данных i
+    double design_matrix[n][degree + 1];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= degree; j++) {
+            design_matrix[i][j] = legendre(j, x[i]);
+        }
+    }
+
+    // Создаем транспонированную матрицу дизайна для дальнейших вычислений
+    double transpose_design[degree + 1][n];
+    for (int i = 0; i <= degree; i++) {
+        for (int j = 0; j < n; j++) {
+            transpose_design[i][j] = design_matrix[j][i];
+        }
+    }
+
+    // Вычисляем матрицу temp, которая является результатом умножения транспонированной матрицы дизайна на матрицу дизайна
+    double temp[degree + 1][degree + 1];
+    for (int i = 0; i <= degree; i++) {
+        for (int j = 0; j <= degree; j++) {
+            temp[i][j] = 0;
+            // Вычисляем каждый элемент матрицы temp по формуле суммы произведений элементов соответствующих строк и столбцов
+            for (int k = 0; k < n; k++) {
+                temp[i][j] += transpose_design[i][k] * design_matrix[k][j];
+            }
+        }
+    }
+
+    // Создаем правую часть системы уравнений, которая является результатом умножения транспонированной матрицы дизайна на вектор y
+    double rhs[degree + 1];
+    for (int i = 0; i <= degree; i++) {
+        rhs[i] = 0;
+        // Вычисляем каждый элемент вектора rhs по формуле суммы произведений элементов соответствующих строки транспонированной матрицы и вектора y
+        for (int j = 0; j < n; j++) {
+            rhs[i] += transpose_design[i][j] * y[j];
+        }
+    }
+
+    // Решаем систему уравнений, вычисляя коэффициенты полинома с помощью метода наименьших квадратов
+    for (int i = 0; i <= degree; i++) {
+        coefficients[i] = 0;
+        // Каждый коэффициент полинома вычисляется как сумма произведений элементов строки матрицы temp и элементов вектора rhs
+        for (int j = 0; j <= degree; j++) {
+            coefficients[i] += temp[i][j] * rhs[j];
+        }
+    }
 }
+
+
 
 // Функция для прогнозирования значений для новых входных данных
 void predict(double *x, double *coefficients, int degree, double *predicted_values, int m) {
-    // Оставляем без изменений, реализация остается та же
-    // ...
+    // Прогнозируем значения для каждой новой точки
+    for (int i = 0; i < m; i++) {
+        predicted_values[i] = 0;  // Инициализируем прогнозируемое значение для текущей точки
+        // Вычисляем прогнозируемое значение как сумму произведений коэффициентов полинома и значений полиномов Лежандра для текущей точки
+        for (int j = 0; j <= degree; j++) {
+            predicted_values[i] += coefficients[j] * legendre(j, x[i]);
+        }
+    }
 }
+
 
 int main() {
     int degree;
